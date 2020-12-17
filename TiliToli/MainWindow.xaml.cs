e24d855ccb3485d9efb224761b8901bd1a26f254
butibence <71645003+butibence@users.noreply.github.com>
2020-12-17 18:40:53 +0100

    TiliToli

---
+++ TiliToli/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TiliToli
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button ezGomb = sender as Button;
+            Button nullaGomb = (Button)FindName("Button0");
+            var fTav = Math.Abs(ezGomb.Margin.Top - nullaGomb.Margin.Top);
+            var vTav = Math.Abs(ezGomb.Margin.Left - nullaGomb.Margin.Left);
+            if ((fTav == 100 && vTav == 0) || (vTav == 100)) 
+            {
+                var seged = ezGomb.Margin;
+                ezGomb.Margin = nullaGomb.Margin;
+                nullaGomb.Margin = seged;
+            }
+        }
+    }
+}
